31169b7b922d68577f48d27b157f43b61c9ce3e0
ayushh93 <ayushkarmacharya220@gmail.com>
2025-01-13 13:35:54 +0545

    feat: Lazy loaded calculator input component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,7 +4,9 @@ import { HomeComponent } from './employees/home/home.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: '/employees', pathMatch: 'full' },
-    // { path: 'employee/:id', component: EmployeeDetailComponent }, 
-    { path: 'calculator', component: CalculatorInputComponent } ,// Calculator route
+    {
+        path: 'calculator',
+        loadComponent: () => import('./calculator-input/calculator-input.component').then(m => m.CalculatorInputComponent)
+    },
     { path: 'employees', loadChildren: () => import('./employees/employees.module').then(m => m.EmployeesModule) },
 ];
